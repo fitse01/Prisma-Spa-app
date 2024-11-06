ef7f122f6a5e7f302a6990bc0771a9a5360209cc
fitse01 <fitsumtafese01@gmail.com>
2024-11-06 10:54:46 +0300

    appointment route added

---
+++ src/routes/appointmentRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {createAppointment,getAllAppointments , getAppointmentById, updateAppointment, deleteAppointment} = require('../controllers/appointmentController');
+
+// Appointment CRUD routes
+router.post('/', createAppointment);
+router.get('/', getAllAppointments);
+router.get('/:id', getAppointmentById);
+router.put('/:id', updateAppointment);
+router.delete('/:id', deleteAppointment);
+
+module.exports = router;
